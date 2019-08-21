a5c768f84eb76ced806d39ddcdb3c869a85af4ac
feng.zhang <cst.feng@gmail.com>
2019-08-21 11:36:28 +0900

    feat: add read.sh

--- hive.sh
+++ hive.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 echo "HIVE_HOME=/root/hive-0.7.0-bin" >> ~/.bash_profile
 echo "PATH=$HIVE_HOME/bin:$PATH" >> ~/.bash_profile
 echo 'export HIVE_HOME PATH' >> ~/.bash_profile
---
+++ read.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+read -p "test me: " yn
+echo $yn
