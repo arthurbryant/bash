b3eb1b9839d7650e7331c69341ddbc4e0601b7f6
arthurbryant <cst.feng@gmail.com>
2019-08-29 07:58:52 +0900

    rm unused

--- confirm_staging_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Push API
-## Android
-curl -F "file=@TestDG.apk" -F "token=000d7468c5f1ea345485030fa7ae50edefcf7a73" -F "message=sample" https://stg.deploygate.com/api/users/arthur/apps
-
-# Invite API
